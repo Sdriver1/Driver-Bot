050d1d86df0ae68e0a356f46f0dfb3eed66faabf
Sdriver1 <sjdriver08@gmail.com>
2022-01-04 22:57:29 -0500

    Testing Deployment

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -32,13 +41,60 @@ const client = new discord_js_1.default.Client({
     ]
 });
 client.on('ready', () => {
+    var _a;
     console.log('The bot is ready!');
+    const guildId = '875594624712966184';
+    const guild = client.guilds.cache.get(guildId);
+    let commands;
+    if (guild) {
+        commands = guild.commands;
+    }
+    else {
+        commands = (_a = client.application) === null || _a === void 0 ? void 0 : _a.commands;
+    }
+    commands === null || commands === void 0 ? void 0 : commands.create({
+        name: 'test',
+        description: 'test command'
+    });
+    commands === null || commands === void 0 ? void 0 : commands.create({
+        name: 'add',
+        description: 'Adds two numbers',
+        options: [
+            {
+                name: 'num1',
+                description: 'The first numbers',
+                required: true,
+                type: discord_js_1.default.Constants.ApplicationCommandOptionTypes.NUMBER
+            },
+            {
+                name: 'num2',
+                description: 'The second number',
+                required: true,
+                type: discord_js_1.default.Constants.ApplicationCommandOptionTypes.NUMBER
+            }
+        ]
+    });
 });
-client.on('messageCreate', (message) => {
-    if (message.content === 'Driver') {
-        message.reply({
-            content: 'Domain!!!',
+client.on('interactionCreate', (interaction) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!interaction.isCommand()) {
+        return;
+    }
+    const { commandName, options } = interaction;
+    if (commandName === 'test') {
+        interaction.reply({
+            content: "This is a test",
         });
     }
-});
+    if (commandName === 'add') {
+        const num1 = options.getNumber('num1');
+        const num2 = options.getNumber('num2');
+        yield interaction.deferReply({
+            ephemeral: true
+        });
+        yield new Promise(resolve => setTimeout(resolve, 5000));
+        interaction.editReply({
+            content: `The sum is ${num1 + num2}`,
+        });
+    }
+}));
 client.login(process.env.TOKEN);
--- index.ts
+++ index.ts
@@ -11,12 +11,67 @@ const client = new DiscordJS.Client({
 
 client.on('ready', () => {
     console.log('The bot is ready!')
+
+    const guildId = '875594624712966184'
+    const guild = client.guilds.cache.get(guildId)
+    let commands
+
+   if (guild) {
+      commands = guild.commands 
+   } else {
+       commands = client.application?.commands
+    }
+
+    commands?.create({
+        name: 'test',
+        description: 'test command'
+    })
+
+    commands?.create({
+        name: 'add',
+        description: 'Adds two numbers',
+        options: [
+            {
+                name:'num1',
+                description: 'The first numbers',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+            },
+            {
+                name: 'num2',
+                description: 'The second number',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+            }
+        ]
+    })
 })
 
-client.on('messageCreate', (message) => {
-    if (message.content === 'Driver') {
-        message.reply({
-            content: 'Domain!!!',
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) {
+      return
+    }
+
+    const { commandName, options } = interaction
+
+    if (commandName === 'test') {
+        interaction.reply({
+            content: "This is a test",
+        })
+    }
+    
+    if (commandName === 'add') {
+        const num1 = options.getNumber('num1')!
+        const num2 = options.getNumber('num2')!
+
+        await interaction.deferReply({
+            ephemeral: true
+        })
+
+        await new Promise(resolve => setTimeout(resolve, 5000))
+
+        interaction.editReply({
+            content: `The sum is ${num1 + num2}`,
         })
     }
 })
