9c835e6d84a05edfbd5a53e6da504ba6a3fa0d34
Sdriver1 <sjdriver08@gmail.com>
2022-01-04 23:42:22 -0500

    Command deploy

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import DiscordJS, { Intents } from 'discord.js'
+import WOKCommands from 'wokcommands'
+import path from 'path'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -12,68 +14,10 @@ const client = new DiscordJS.Client({
 client.on('ready', () => {
     console.log('The bot is ready!')
 
-    const guildId = '875594624712966184'
-    const guild = client.guilds.cache.get(guildId)
-    let commands
-
-   if (guild) {
-      commands = guild.commands 
-   } else {
-       commands = client.application?.commands
-    }
-
-    commands?.create({
-        name: 'test',
-        description: 'test command'
+    new WOKCommands(client,{
+commandsDir: path.join(__dirname, 'commands'),
+typeScript: true
     })
-
-    commands?.create({
-        name: 'add',
-        description: 'Adds two numbers',
-        options: [
-            {
-                name:'num1',
-                description: 'The first numbers',
-                required: true,
-                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
-            },
-            {
-                name: 'num2',
-                description: 'The second number',
-                required: true,
-                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
-            }
-        ]
-    })
-})
-
-client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isCommand()) {
-      return
-    }
-
-    const { commandName, options } = interaction
-
-    if (commandName === 'test') {
-        interaction.reply({
-            content: "This is a test",
-        })
-    }
-    
-    if (commandName === 'add') {
-        const num1 = options.getNumber('num1')!
-        const num2 = options.getNumber('num2')!
-
-        await interaction.deferReply({
-            ephemeral: true
-        })
-
-        await new Promise(resolve => setTimeout(resolve, 5000))
-
-        interaction.editReply({
-            content: `The sum is ${num1 + num2}`,
-        })
-    }
 })
 
 client.login(process.env.TOKEN)
